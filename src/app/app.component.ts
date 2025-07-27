f34567216e2ea0ff9cc02b03fb038786202b40c7
farzadmsv14 <farzadmsv14@gmail.com>
2025-07-27 16:13:12 +0330

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'takePicture';
+  @ViewChild('video') video!: ElementRef<HTMLVideoElement>;
+  stream!: MediaStream;
+  photo: string | null = null;
+  streamStarted = false;
+
+  async startCamera() {
+    try {
+      this.stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      this.video.nativeElement.srcObject = this.stream;
+      this.streamStarted = true;
+    } catch (error) {
+      console.error('دسترسی به دوربین ممکن نیست:', error);
+    }
+  }
+
+  capture() {
+    const videoElement = this.video.nativeElement;
+    const canvas = document.createElement('canvas');
+    canvas.width = videoElement.videoWidth;
+    canvas.height = videoElement.videoHeight;
+    canvas.getContext('2d')?.drawImage(videoElement, 0, 0);
+
+    this.photo = canvas.toDataURL('image/png'); 
+  }
 }
